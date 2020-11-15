2d629ce0df9813c23614b95f5c5cd9cd9b04373c
sjemsek <s3468643@student.rmit.edu.au>
2020-11-15 23:57:21 +1100

    testing status is set correctly

--- test/payment/CalculatePaymentTest.java
+++ test/payment/CalculatePaymentTest.java
@@ -1,22 +1,48 @@
 package payment;
 
+import io.CSVReader;
+import model.CompleteTrip;
+import model.TripPart;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatePaymentTest {
+    CSVReader csvReader = new CSVReader();
+    Map<String , TripPart> tripParts = csvReader.read("input.csv");
+    @BeforeEach
+    public void setup() {
+
+    }
 
     @Test
     void getTripType() {
     }
 
     @Test
-    public void shouldReturnFareWhenValid(){
-        
+    public void shouldHaveCompleteStatusWhenValid(){
+
+        tripParts.get("0").setTapType("ON");
+        tripParts.get("1").setTapType("OFF");
+
+        Map<String, CompleteTrip> completeTrips = CalculatePayment.getTripType(tripParts);
+
+        assertEquals(completeTrips.get("1").getStatus(), CompleteTrip.Status.COMPLETE);
+
     }
 
     @Test
-    public void shouldCalculateMaxWhenInvalid(){
+    public void shouldHaveIncompleteStatusWhenInvalid(){
+        tripParts.get("0").setTapType("ON");
+        tripParts.get("1").setTapType("ON");
+
+        Map<String, CompleteTrip> completeTrips = CalculatePayment.getTripType(tripParts);
+
+        assertEquals(completeTrips.get("1").getStatus(), CompleteTrip.Status.INCOMPLETE);
 
     }
 }
