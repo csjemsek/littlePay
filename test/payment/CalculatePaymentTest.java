bee1e13fb621c87fc9711f790afe0fb1ab9b4b83
sjemsek <s3468643@student.rmit.edu.au>
2020-11-16 00:57:08 +1100

    added further validation of trip calculation behaviour

--- test/payment/CalculatePaymentTest.java
+++ test/payment/CalculatePaymentTest.java
@@ -6,6 +6,7 @@ import model.TripPart;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +26,6 @@ class CalculatePaymentTest {
 
     @Test
     public void shouldHaveCompleteStatusWhenValid(){
-
         tripParts.get("0").setTapType("ON");
         tripParts.get("1").setTapType("OFF");
 
@@ -45,4 +45,41 @@ class CalculatePaymentTest {
         assertEquals(completeTrips.get("1").getStatus(), CompleteTrip.Status.INCOMPLETE);
 
     }
+
+    @Test
+    public void shouldCalculateDuration(){
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime plusFive = now.plusSeconds(5);
+        tripParts.get("0").setDateTimeUTC(now);
+        tripParts.get("1").setDateTimeUTC(now);
+
+        Map<String, CompleteTrip> completeTripsZero = CalculatePayment.getTripType(tripParts);
+        assertEquals(0.0, completeTripsZero.get("1").getDuration());
+
+        tripParts.get("1").setDateTimeUTC(plusFive);
+        Map<String, CompleteTrip> completeTripFive = CalculatePayment.getTripType(tripParts);
+
+        assertEquals(5.0, completeTripFive.get("1").getDuration());
+    }
+
+    @Test
+    public void shouldCalculateMaxPriceInvalidTripStop1(){
+        tripParts.get("0").setTapType("ON");
+        tripParts.get("1").setTapType("ON");
+
+        tripParts.get("0").setStopId("Stop1");
+
+        Map<String, CompleteTrip> completeTripStop1 = CalculatePayment.getTripType(tripParts);
+        assertEquals(7.3, completeTripStop1.get("1").getChargeAmount());
+
+        tripParts.get("0").setStopId("Stop2");
+        Map<String, CompleteTrip> completeTripStop2 = CalculatePayment.getTripType(tripParts);
+
+        assertEquals(5.5, completeTripStop2.get("1").getChargeAmount());
+
+        tripParts.get("0").setStopId("Stop3");
+        Map<String, CompleteTrip> completeTripStop3 = CalculatePayment.getTripType(tripParts);
+        assertEquals(7.3, completeTripStop3.get("1").getChargeAmount());
+    }
+
 }
