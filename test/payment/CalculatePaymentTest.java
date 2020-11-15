c36884f32cc21c63bb497066a0a83c92974b10aa
sjemsek <s3468643@student.rmit.edu.au>
2020-11-15 23:03:07 +1100

    defining behaviour for payment calculation

---
+++ test/payment/CalculatePaymentTest.java
@@ -0,0 +1,22 @@
+package payment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatePaymentTest {
+
+    @Test
+    void getTripType() {
+    }
+
+    @Test
+    public void shouldReturnFareWhenValid(){
+        
+    }
+
+    @Test
+    public void shouldCalculateMaxWhenInvalid(){
+
+    }
+}
